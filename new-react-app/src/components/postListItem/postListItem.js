e180b5276512e0e8a7aebaf89654d757c2c0d8f7
roman.kostin <rko43@mail.ru>
2020-06-24 19:05:43 +0300

    create components

--- new-react-app/src/components/postListItem/postListItem.js
+++ new-react-app/src/components/postListItem/postListItem.js
@@ -6,10 +6,10 @@ const PostListItem = () => {
     <li className="app-list-item d-flex justify-content-between">
       <span className="app-list-item-label"> Hello World</span>
       <div className="d-flex justify-content-center align-items-center">
-        <button className="btn-star btn-sm">
+        <button type="button" className="btn-star btn-sm">
           <i className="fa fa-star-o"></i>
         </button>
-        <button className="btn-trash btn-sm">
+        <button type="button" className="btn-trash btn-sm">
           <i className="fa fa-trash-o"></i>
         </button>
         <i className="fa fa-heart"></i>
